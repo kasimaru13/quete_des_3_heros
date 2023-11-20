b2ab2203e162042198154d1b0bdcdd1607a90f46
kasimaru13 <kasimaru13@gmail.com>
2023-11-20 14:03:47 +0100

    début com

--- src/Factory/ConcreteCreatorMonster.java
+++ src/Factory/ConcreteCreatorMonster.java
@@ -3,6 +3,10 @@ package Factory;
 import Elements.Element;
 import Elements.Monster;
 
+
+/**
+ * Créateur concret pour les Monstres (factory design pattern)
+ */
 public class ConcreteCreatorMonster extends Creator {
     public Element createElement() {
         return new Monster();
--- src/Factory/ConcreteCreatorObstacle.java
+++ src/Factory/ConcreteCreatorObstacle.java
@@ -3,6 +3,9 @@ package Factory;
 import Elements.Element;
 import Elements.Obstacle;
 
+/**
+ * Créateur concret pour les Obstacles (factory design pattern)
+ */
 public class ConcreteCreatorObstacle extends Creator {
     public Element createElement(){
         return new Obstacle();
