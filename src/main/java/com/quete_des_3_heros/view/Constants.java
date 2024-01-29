bdfd1666ecbf4daa6b30a59f2e5d26877de1142c
kasimaru13 <kasimaru13@gmail.com>
2024-01-29 16:07:03 +0100

    window size fix

--- src/main/java/com/quete_des_3_heros/view/Constants.java
+++ src/main/java/com/quete_des_3_heros/view/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
     public static final int SPRITE_SIZE = 48;
     public static final int BOARD_SIZE = NUMBER_OF_SQUARES * SPRITE_SIZE + (NUMBER_OF_SQUARES - 1) * GRID; // 15 is for the grid (there are 15 lines, each of 1 pixel)
     public static final int WINDOW_WIDTH = 16 * 48 + 2 * 280;
-    public static final int WINDOW_HEIGHT = 16 * 48;
+    public static final int WINDOW_HEIGHT = 16 * 48 + (16 - 1) * GRID;
     public static final int LEFTPANEL_WIDTH = 280;
     public static final int RIGHTPANEL_WIDTH = 280;
 
